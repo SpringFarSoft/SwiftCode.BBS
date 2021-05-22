52468b28d0a568bc0b820126f6d50552f43971ee
HDONG <377749229@qq.com>
2021-05-22 15:15:20 +0800

    采用程序集批量注入

--- SwiftCode.BBS.Extensions/ServiceExtensions/AutofacModuleRegister.cs
+++ SwiftCode.BBS.Extensions/ServiceExtensions/AutofacModuleRegister.cs
@@ -6,6 +6,8 @@ using System.Text;
 using Autofac;
 using Microsoft.EntityFrameworkCore;
 using SwiftCode.BBS.EntityFramework;
+using SwiftCode.BBS.IServices;
+using SwiftCode.BBS.Services;
 
 namespace SwiftCode.BBS.Extensions.ServiceExtensions
 {
@@ -13,44 +15,28 @@ namespace SwiftCode.BBS.Extensions.ServiceExtensions
     {
         protected override void Load(ContainerBuilder builder)
         {
-
-            //builder.Register(c =>
-            //{
-            //    var optionsBuilder = new DbContextOptionsBuilder<SwiftCodeBbsContext>();
-            //    optionsBuilder.UseSqlServer(@"Server=.; Database=SwiftCodeBbs; Trusted_Connection=True; Connection Timeout=600;MultipleActiveResultSets=true;", b => b
-            //        .MigrationsAssembly("SwiftCode.BBS.EntityFramework"));
-            //    return optionsBuilder.Options;
-            //}).InstancePerLifetimeScope();
-            //// 注册 DbContext
-            //builder.RegisterType<SwiftCodeBbsContext>()
-            //    .AsSelf()
-            //    .InstancePerLifetimeScope();
-            // scan all assemblies in current application domain and resolve them on convention
-            //builder.RegisterAssemblyTypes(AppDomain.CurrentDomain.GetAssemblies())
-            //    .AsImplementedInterfaces();
-
             var basePath = AppContext.BaseDirectory;
 
-            // builder.RegisterType<ArticleService>().As<IArticleService>();
+            builder.RegisterType<ArticleService>().As<IArticleService>();
 
-            //var assemblysServices = Assembly.Load("SwiftCode.BBS.Services");//要记得!!!这个注入的是实现类层，不是接口层！不是 IServices
-            //builder.RegisterAssemblyTypes(assemblysServices).AsImplementedInterfaces();//指定已扫描程序集中的类型注册为提供所有其实现的接口。
-            //var assemblysRepository = Assembly.Load("SwiftCode.BBS.Repositories");//模式是 Load(解决方案名)
-            //builder.RegisterAssemblyTypes(assemblysRepository).AsImplementedInterfaces();
-
-
-            var servicesDllFile = Path.Combine(basePath, "SwiftCode.BBS.Services.dll");
-            var repositoryDllFile = Path.Combine(basePath, "SwiftCode.BBS.Repositories.dll");
-            if (!(File.Exists(servicesDllFile) && File.Exists(repositoryDllFile)))
-            {
-                var msg = "Repositories.dll和Services.dll 丢失。";
-                throw new Exception(msg);
-            }
-            var assemblysServices = Assembly.LoadFrom(servicesDllFile);
-            builder.RegisterAssemblyTypes(assemblysServices).AsImplementedInterfaces();
-
-            var assemblysRepository = Assembly.LoadFrom(repositoryDllFile);
+            var assemblysServices = Assembly.Load("SwiftCode.BBS.Services");//要记得!!!这个注入的是实现类层，不是接口层！不是 IServices
+            builder.RegisterAssemblyTypes(assemblysServices).AsImplementedInterfaces();//指定已扫描程序集中的类型注册为提供所有其实现的接口。
+            var assemblysRepository = Assembly.Load("SwiftCode.BBS.Repositories");//模式是 Load(解决方案名)
             builder.RegisterAssemblyTypes(assemblysRepository).AsImplementedInterfaces();
+
+
+            //var servicesDllFile = Path.Combine(basePath, "SwiftCode.BBS.Services.dll");
+            //var repositoryDllFile = Path.Combine(basePath, "SwiftCode.BBS.Repositories.dll");
+            //if (!(File.Exists(servicesDllFile) && File.Exists(repositoryDllFile)))
+            //{
+            //    var msg = "Repositories.dll和Services.dll 丢失。";
+            //    throw new Exception(msg);
+            //}
+            //var assemblysServices = Assembly.LoadFrom(servicesDllFile);
+            //builder.RegisterAssemblyTypes(assemblysServices).AsImplementedInterfaces();
+
+            //var assemblysRepository = Assembly.LoadFrom(repositoryDllFile);
+            //builder.RegisterAssemblyTypes(assemblysRepository).AsImplementedInterfaces();
 
         }
     }
